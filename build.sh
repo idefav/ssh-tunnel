e0a5ccfbada4c65430d74ed2b8e3e99b0a1e8527
wuzishu <wzs2009_hi@126.com>
2022-02-27 15:26:11 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+GOOS=windows GOARCH=amd64 go build -o bin/ssh-tunnel-amd64.exe
+
+GOOS=windows GOARCH=386 go build -o bin/ssh-tunnel-386.exe
+
+GOOS=darwin GOARCH=amd64 go build -o bin/ssh-tunnel-amd64-darwin
+
+GOOS=linux GOARCH=amd64 go build -o bin/ssh-tunnel-amd64-linux
