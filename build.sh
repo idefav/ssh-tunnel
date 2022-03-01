280138d8906bc077478caf6a3db355fe88400330
idefav <wzs2009_hi@126.com>
2022-03-01 09:39:47 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ GOOS=windows GOARCH=386 go build -o bin/ssh-tunnel-386.exe
 GOOS=darwin GOARCH=amd64 go build -o bin/ssh-tunnel-amd64-darwin
 
 GOOS=linux GOARCH=amd64 go build -o bin/ssh-tunnel-amd64-linux
+
+GOOS=darwin GOARCH=arm64 go build -o bin/ssh-tunnel-arm64-darwin
