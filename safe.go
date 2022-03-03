174d1a527d1ff9a022e28b05ff3c68a8757d569f
idefav <wzs2009_hi@126.com>
2022-03-03 12:17:22 +0800

    Create safe.go

---
+++ safe.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"runtime/debug"
+)
+
+func GO(fn func()) {
+	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Panic(fmt.Sprintf("Go panic:%v \n%s", err, debug.Stack()))
+			}
+		}()
+		fn()
+	}()
+}
